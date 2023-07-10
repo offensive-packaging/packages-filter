6b4d1bd3da89b8939f0f2af09241a2fc5bf61a29
sulaiman-coder <51925710+sulaiman-coder@users.noreply.github.com>
2023-07-10 08:32:25 -0700

    Create main.go

---
+++ main.go
@@ -0,0 +1,35 @@
+package main
+
+import (
+	"log"
+	"package-filter/fileops"
+	"package-filter/filter"
+)
+
+// Here the three phases of the program are carried out:
+// 1. Download the Packages for all the architectures.
+// 2. Filter and return them as JSON files (~70 mb).
+func main() {
+	f := new(filter.Package)
+
+	// Create temporary dir called "packages"
+	fileops.Mkdir()
+
+	// Start the downloading phase
+	log.Println("[info] Downloading packages...")
+
+	// Use the DownloadPackages function to download Packages for each branch and architecture
+	fileops.GetJSONPackages()
+
+	// The filter phase begins.
+	log.Println("[info] Filtering...")
+	f.Parser()
+
+	// The packages folder which contains Packages for each architecture
+	// is deleted as it is no longer useful.
+	fileops.Rmdirs()
+
+	log.Println("[info] All Packages files deleted.")
+	log.Println("[success] Check the json folder.")
+}
+
